ba3bff93714f1dfd86fcf28762f95da5f9797865
karam96gh <karam96gh@gmail.com>
2025-03-12 04:13:52 -0700

    dslkvsd

--- src/Routes/upload_file.js
+++ src/Routes/upload_file.js
@@ -1,13 +1,18 @@
 const { Router } = require('express');
 const multer = require('multer'); // Import multer here
 const { uploadImage } = require('../controllers/upload_file');
+const fs = require('fs');
+const path = require('path');
 
 const router = Router();
 
 // Configure multer for image uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '/src/images/'); // Set the destination directory for uploaded images
+     const uploadPath = path.join(__dirname, '../controllers/src/images/');
+            fs.mkdirSync(uploadPath, { recursive: true }); // التأكد من إنشاء المجلد تلقائيًا
+            cb(null, uploadPath);
+
   },
   filename: (req, file, cb) => {
     if(file.originalname.endsWith('mp4'))
